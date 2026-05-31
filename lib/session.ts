f56aa921877bebd4bf6e3b5ec951b14df5fbd03b
Bigbattery <86228691+bigbattery-git@users.noreply.github.com>
2026-05-31 15:49:27 +0900

    chore : 세션 설정 파일 생성

---
+++ lib/session.ts
@@ -0,0 +1,35 @@
+import { getIronSession, SessionOptions } from "iron-session";
+import { cookies } from "next/headers";
+
+export type SessionUser = {
+    id : number
+}
+
+export type SessionData = {
+    user ?: SessionUser,
+    isLoggedin ?: boolean
+}
+
+const sessionPassword = process.env.SESSION_PASSWORD;
+
+if(!sessionPassword || sessionPassword.length < 32){
+    throw new Error("세션 비번이 없거나 32자 미만임");
+}
+
+export const sessionOptions : SessionOptions = {
+    password : sessionPassword,
+    cookieName : "session_data",
+    ttl : 60 * 60 * 2,
+    cookieOptions : {
+        httpOnly : true,
+        secure : process.env.NODE_ENV === "production",
+        sameSite : "lex",
+        path : "/"
+    }
+}
+
+export async function getSession() {
+    const cookieStore = await cookies();
+
+    return getIronSession<SessionData>(cookieStore, sessionOptions);
+}
