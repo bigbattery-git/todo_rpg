210022110eded7dd2b83b3cc83e52c64f12c31af
Bigbattery <86228691+bigbattery-git@users.noreply.github.com>
2026-05-31 20:21:10 +0900

    chore : 레벨업에 필요한 경험치 로직 미리 추가

--- lib/Config.ts
+++ lib/Config.ts
@@ -4,3 +4,9 @@ export const NEEDLOGIN : string = "로그인이 필요합니다";
 
 // TODO : 모델의 enum값 바뀔 때 값이 바뀌도록, 여기서 별도로 한 번 더 추가할 필요 없도록 리팩토링 하기
 export const TODOTYPE : string[] = ["WORK", "HOBBY", "HEALTH", "SELP_MANAGEMENT", "STUDY"];
+
+export const getNextLevelUpExp = (currentExt : number) : number => {
+    const currentLevel : number = Math.floor(currentExt / 1000);
+
+    return currentLevel * 1000;
+}
