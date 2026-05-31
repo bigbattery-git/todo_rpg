8198fe7ca4dd491de4117bdf736f42fac93bf026
Bigbattery <86228691+bigbattery-git@users.noreply.github.com>
2026-05-31 16:04:54 +0900

    chore : 초기 테스트용 시더 생성

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,6 +7,7 @@ import { defineConfig, env } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
+    seed: "tsx prisma/seeder.ts",
     path: "prisma/migrations",
   },
   engine: "classic",
---
+++ prisma/seeder.ts
@@ -0,0 +1,41 @@
+import { prisma } from "@/lib/prisma"
+
+async function main(){
+    const user = await prisma.user.upsert({
+        where : {
+            email : "test@test.com"
+        },
+        update: {},
+        create : {
+            name : "홍길동",
+            email : "test@test.com",
+            password : "qwer1234"
+        }
+    })
+
+    const reward = await prisma.reward.upsert({
+        where : {
+            id : 1
+        }, 
+        update : {},
+        create : {
+            name : "데일리 퀘스트 달성!",
+            content : "데일리 퀘스트를 모두 달성했어요!",
+            memo : "기본 완료 목록"
+        }
+    })
+
+    const rewardItem = await prisma.rewardItem.upsert({
+        where : {
+            id : 1
+        }, update : {
+
+        }, create : {
+            name : "기본적인 아이템",
+            content : "기본적인 아이템이라 뭐라 닥히 할 말은 없음",
+            memo : "실수로 null하게 못만들었음"
+        }
+    })
+}
+
+main().then(() => {console.log("작업 끝")}).catch((e) => {console.error(e)});
