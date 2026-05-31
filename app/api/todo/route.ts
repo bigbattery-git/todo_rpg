36d16eb585b2e19ef971fdfbbabc0a33f0f42f4e
Bigbattery <86228691+bigbattery-git@users.noreply.github.com>
2026-05-31 19:02:27 +0900

    feat : 기본 할 일 목록 추가하기

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -1,4 +1,4 @@
-import { SERVERERROR } from "@/lib/Config";
+import { BADJSONDATA, SERVERERROR, TODOTYPE } from "@/lib/Config";
 import { prisma } from "@/lib/prisma";
 import { getSession } from "@/lib/session";
 import { TodoStatus, TodoType} from "@/src/generated/enums";
@@ -20,6 +20,12 @@ interface GETTodoResponse {
         todoData : TodoData[],
         lastPage : number
     }
+}
+
+interface POSTTodoResponse {
+    success : boolean,
+    message : string | null,
+    data ?: TodoData
 }
 
 export async function GET(req : NextRequest){
@@ -71,8 +77,86 @@ export async function GET(req : NextRequest){
             lastPage : Math.ceil(totalCount/pageSize)
         }
 
+        response.success = false;
+        response.message = "TODO 목록 조회를 완료하였습니다";
+
         return NextResponse.json(response, {status : 200});
 
+    } catch (e) {
+        console.error(e);
+        response.message = SERVERERROR;
+        return NextResponse.json(response, {status : 500});
+    }
+}
+
+export async function POST(req : NextRequest){
+    let requestData;
+
+    const todoType : string[] = TODOTYPE;
+    const response : POSTTodoResponse = {
+        success : false,
+        message : null
+    }
+
+    const session = await getSession();
+
+    if(!session || !session.isLoggedin || !session.user?.id){
+        response.message = "로그인이 필요합니다";
+        return NextResponse.json(response, {status : 401});
+    }
+
+    try {
+        requestData = await req.json()
+    } catch {
+        response.message = BADJSONDATA;
+        return NextResponse.json(response, {status : 400});
+    }
+
+    console.log("POST /api/todo req data:", JSON.stringify(requestData));
+
+    if(!requestData.title || !requestData.content || !requestData.todoType){
+        response.message = "제목, 설명, 타입이 필요합니다";
+        return NextResponse.json(response, {status : 400});
+    }
+
+    if(requestData.title > 50){
+        response.message = "제목 길이는 50자를 넘지 않게 해주세요";
+        return NextResponse.json(response, {status : 400});
+    }
+
+    if(requestData.content > 800){
+        response.message = "설명은 800자를 안넘도록 해주세요";
+        return NextResponse.json(response, {status : 400});
+    }
+
+    let hasTodoType = false;
+    todoType.forEach(a => {
+        if(requestData.todoType === a){
+            hasTodoType = true;
+        }
+    });
+    
+    if(!hasTodoType){
+        response.message = "선택 가능한 할 일 카테고리를 선택하세요";
+        return NextResponse.json(response, {status : 400});
+    }
+
+    try {
+        const todo = await prisma.todo.create({
+            data : {
+                title : requestData.title,
+                content : requestData.content,
+                todoStatus : "PENDING",
+                todoType : requestData.todoType,
+                userId : session.user?.id
+            }
+        });
+
+        response.success = true;
+        response.message = "todo를 추가했습니다."
+        response.data = todo;
+        
+        return NextResponse.json(response, {status : 200});
     } catch (e) {
         console.error(e);
         response.message = SERVERERROR;
--- lib/Config.ts
+++ lib/Config.ts
@@ -1,3 +1,5 @@
 export const BADJSONDATA : string= "잘못된 json입니다.";
 export const SERVERERROR : string = "서버 오류가 발생했습니다";
 
+// TODO : 모델의 enum값 바뀔 때 값이 바뀌도록, 여기서 별도로 한 번 더 추가할 필요 없도록 리팩토링 하기
+export const TODOTYPE : string[] = ["WORK", "HOBBY", "HEALTH", "SELP_MANAGEMENT", "STUDY"];
