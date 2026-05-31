08290702a429cb4549135420e3aa97ac2c855fad
Bigbattery <86228691+bigbattery-git@users.noreply.github.com>
2026-05-31 17:10:16 +0900

    feat : 로그인 api 완성

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,68 @@
+import { getSession } from "@/lib/session";
+import { NextRequest, NextResponse } from "next/server";
+import { BADJSONDATA, SERVERERROR } from "@/lib/Config";
+import { prisma } from "@/lib/prisma";
+
+interface POSTLoginResponse{
+    success : boolean,
+    message : string | null 
+}
+
+export async function POST(req : NextRequest){
+    let requestData;
+
+    const response : POSTLoginResponse = {
+        success : false,
+        message : null
+    }
+
+    try {
+        requestData = await req.json();
+    } catch (e) {
+        response.message = BADJSONDATA;
+        return NextResponse.json(response, {status : 400});
+    }
+
+    if(!requestData.email){
+        response.message = "이메일을 입력하세요";
+        return NextResponse.json(response, {status : 400});
+    }
+
+    if(!requestData.password){
+        response.message = "비밀번호를 입력하세요";
+        return NextResponse.json(response, {status : 400});
+    }
+
+    try{
+        const userData = await prisma.user.findFirst({
+            where:{
+                email : requestData.email
+            }
+        });
+
+        response.message = "아이디 또는 비밀번호가 맞지 않습니다.";
+
+        if(!userData){
+            return NextResponse.json(response, {status : 404});
+        }
+
+        if(userData.password !== requestData.password){
+            return NextResponse.json(response, {status : 404});
+        }
+
+        const session = await getSession();
+        session.user = {
+            id : userData.id
+        }
+        await session.save();
+
+        response.success = true;
+        response.message = "로그인에 성공했습니다";
+
+        return NextResponse.json(response, {status : 200});
+    } catch(e) {
+        console.error(e);
+        response.message = SERVERERROR;
+        return NextResponse.json(response, {status : 500});
+    }
+}
---
+++ lib/Config.ts
@@ -0,0 +1,2 @@
+export const BADJSONDATA = "잘못된 json입니다.";
+export const SERVERERROR = "서버 오류가 발생했습니다";
