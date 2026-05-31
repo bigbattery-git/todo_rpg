04b0fc01ae7785436b71f250d8ce6a25bde81768
Bigbattery <86228691+bigbattery-git@users.noreply.github.com>
2026-05-31 18:41:19 +0900

    fix : cofig에 타입 추가

--- lib/Config.ts
+++ lib/Config.ts
@@ -1,2 +1,3 @@
-export const BADJSONDATA = "잘못된 json입니다.";
-export const SERVERERROR = "서버 오류가 발생했습니다";
+export const BADJSONDATA : string= "잘못된 json입니다.";
+export const SERVERERROR : string = "서버 오류가 발생했습니다";
+
